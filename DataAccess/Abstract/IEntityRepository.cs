93a7c8c6db31fb0970aca2adee2ba55f69750f90
ferhatarkan <55208799+ferhatarkan@users.noreply.github.com>
2021-02-04 16:39:17 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic Constraint
+    //Class:referans tip
+    //new() new edilebilir olmalı
+    //IEntity: IEntity olabilir veya Ientity implement eden bir sınıf olabilir
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>>filter=null);
+        T Get(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Interfacin operasyonlar  public
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        
+    }
+}
