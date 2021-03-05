1352ce17d7eb312c5e6bc4f6429422680aa277ec
ferhatarkan <55208799+ferhatarkan@users.noreply.github.com>
2021-03-05 23:20:33 +0300

    05.03.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
@@ -28,10 +29,12 @@ namespace Business.Concrete
             _categoryService = categoryService;
 
         }
+        //Clain
+        [SecuredOperation("product,add")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-
+            
             //Eğer mevcut kategori sayisi 15i geçtiyse sisteme yeni ürün eklenemez.
             //Aynı isimde ürün eklenemez
             IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
